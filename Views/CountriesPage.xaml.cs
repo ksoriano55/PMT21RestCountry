5a6808632b848a7cc264dadfc1b32cde3ca0800b
Keyla Soriano <44504724+ksoriano55@users.noreply.github.com>
2024-07-09 23:31:13 -0600

    Listado de paises

--- Views/CountriesPage.xaml.cs
+++ Views/CountriesPage.xaml.cs
@@ -6,4 +6,26 @@ public partial class CountriesPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        Countries.ItemsSource = await Controllers.ContriesController.GetCountries();
+    }
+
+    private void Countries_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_1(object sender, TappedEventArgs e)
+    {
+
+    }
 }
